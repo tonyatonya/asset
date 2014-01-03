f66d2f0086e8d305eadfd40a00d5ba53d6d3cc87
Athapong Athamasirigun <tonya_0@hotmail.com>
2014-01-03 11:09:35 +0700

    Complete Submenu

---
+++ inc_member-leftcol.php
@@ -0,0 +1,19 @@
+<div class="category-menu">
+	<div class="title"><h2>สมาชิก</h2></div>
+	<ul>
+		<li><a href="#"><div class="ico"><img src="images/exampleicon.png" alt="exampleicon" width="46" height="46" /></div><div>ข้อมูลส่วนตัว</div></a></li>
+		<li><a href="#"><div class="ico"><img src="images/exampleicon.png" alt="exampleicon" width="46" height="46" /></div><div>เปลี่ยนรหัสผ่าน</div></a></li>
+	</ul>
+</div>
+<?php require('inc_calculatebanner.php'); ?>
+<div class="left-banner">
+			<a href="#">
+				<img src="images/ebookbanner.jpg" alt="ebookbanner" width="299" height="110">
+			</a>
+		</div>
+<div class="service-box">
+			<div class="service-title">ลิงค์หน่วยงานต่างๆ</div>
+			<div class="service-body">
+				 
+			</div>
+		</div>
